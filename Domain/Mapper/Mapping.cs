b9913570bd9db2a5d5dae8cb613b18d8f59e3f71
Irina Haidzel <scorpion_nova@tut.by>
2020-01-04 23:51:42 +0300

    Mapping

---
+++ Domain/Mapper/Mapping.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AutoMapper;
+
+namespace Domain.Mapper
+{
+    class Mapping
+    {
+        private readonly MapperConfiguration TOADAPTERMAPPERCFG;
+        private readonly MapperConfiguration FROMADAPTERMAPPERCFG;
+
+
+
+
+    }
+}
