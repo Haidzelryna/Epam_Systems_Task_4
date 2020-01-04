913123ff3a65168210c6e2f75bf2f06bb41a825d
Irina Haidzel <scorpion_nova@tut.by>
2020-01-04 17:36:32 +0300

    Repository start

---
+++ Domain/Repository/GenericRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Repository
+{
+    public class GenericRepository<Entity, IEntity> : IGenericRepository<Entity, IEntity> //where Entity : class, interface        
+    {
+        public void Add(IEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(IEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<IEntity> SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Domain/Repository/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace Domain.Repository
+{
+    public interface IGenericRepository<Entity, IEntity>: IEntity where Entity: IEntity
+    {
+        //Get
+
+        void Add(IEntity entity);
+
+        void Delete(IEntity Entity);
+
+        IQueryable<IEntity> SaveChanges();
+    }
+}
