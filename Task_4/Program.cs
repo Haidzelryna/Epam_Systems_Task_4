eec61eb3aae3996440bbc8e8e0c9ae97ca772c89
Irina Haidzel <scorpion_nova@tut.by>
2020-01-08 11:50:34 +0300

    static void Main(string[] args)

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -5,12 +5,8 @@ namespace Task_4
 {
     class Program
     {
-        private const string ADMINID = "80AB7036-5D4A-11E6-9903-0050569977A1";
-        private static Guid adminGuid;
-
         static void Main(string[] args)
-        {
-           
+        {          
             MainMethods.watcherCreated();
 
             //Стартовые данные, заполняем БД
